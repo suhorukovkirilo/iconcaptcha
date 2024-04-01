74298391aa748660f583295b32a01cb1f6047169
kirill <98096381+suhorukovkirilo@users.noreply.github.com>
2024-04-01 21:45:44 +0200

    MVP

--- captcha.js
+++ captcha.js
@@ -1,6 +1,6 @@
 const RandInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-function genPattern() {
+function getPattern() {
     var patterns = [
         [0, 1, 1, 1, 1],
         [1, 0, 1, 1, 1],
@@ -60,3 +60,58 @@ function genPattern() {
 
     return patterns[randNum]
 };
+
+function getIcon() {
+    return 'https://raw.githubusercontent.com/suhorukovkirilo/iconcaptcha/main/icons/' + RandInt(1, 140).toString() + '.png'
+};
+
+function Captcha(onfail, onsuccess) {
+    function success() {
+        onsuccess();
+    };
+
+    function fail() {
+        onfail();
+    };
+
+    var dialog = document.createElement('dialog');
+    var text = document.createElement('p');
+    var captcha = document.createElement('div');
+
+    var a1 = document.createElement('img');
+    var a2 = document.createElement('img');
+    var a3 = document.createElement('img');
+    var a4 = document.createElement('img');
+    var a5 = document.createElement('img');
+
+    captcha.appendChild(a1);
+    captcha.appendChild(a2);
+    captcha.appendChild(a3);
+    captcha.appendChild(a4);
+    captcha.appendChild(a5);
+
+    dialog.appendChild(text);
+    dialog.appendChild(captcha);
+
+    document.body.appendChild(dialog);
+
+    var icons = [getIcon(), getIcon(), getIcon()];
+    var images = [a1, a2, a3, a4, a5];
+
+    var pattern = getPattern();
+
+    for (var i of [0, 1, 2, 3, 4]) {
+        var image = images[i];
+        var src = icons[pattern[i]];
+        image.src = src;
+        if (pattern[i] === 0) {
+            image.addEventListener('click', success);
+        } else {
+            image.addEventListener('click', fail);
+        };
+    };
+
+    dialog.showModal();
+};
+
+Captcha(function(){console.log("fail")}, function(){console.log("success")});
