92cfed82297de13e2a033a5f967f2b1bf0c0d260
kirill <98096381+suhorukovkirilo@users.noreply.github.com>
2024-04-02 15:18:02 +0200

    Add pre captcha

--- captcha.js
+++ captcha.js
@@ -160,6 +160,90 @@ function Icon(id) {
 };
 
 function Captcha(onfail, onsuccess, onclose) {
+    var dialog = document.createElement('dialog');
+    dialog.id = 'IconCaptchaDialog';
+    dialog.style.backgroundColor = '#4c4c4c';
+    dialog.style.border = '3px solid rgba(0, 0, 0, 0.2)';
+    dialog.style.borderRadius = '15px';
+    dialog.style.padding = '10px';
+    dialog.style.paddingLeft = '25px';
+    dialog.style.paddingRight = '25px';
+    dialog.style.width = '340px';
+    dialog.style.height = '70px';
+
+    var close = document.createElement('img');
+    close.src = Icon('close');
+    close.style.width = '16px';
+    close.style.height = '16px';
+    close.style.position = 'relative';
+    close.style.top = '0.1%';
+    close.style.left = '107%';
+    close.addEventListener('click', function() {
+        dialog.close();
+        onclose();
+    });
+
+    var copyright = document.createElement('div');
+    copyright.innerHTML = '©Icon Captcha';
+    copyright.style.position = 'relative';
+    copyright.style.top = '0.1%';
+    copyright.style.left = '-7%';
+    copyright.style.color = 'rgba(255, 255, 255, 0.25)';
+
+    var load = document.createElement('div');
+    load.style.backgroundColor = '#4c4c4c';
+    load.style.borderRadius = '50%';
+    load.style.border = '6px solid #52b3d9';
+    load.style.width = '24px';
+    load.style.height = '24px';
+    load.style.position = 'absolute';
+    load.style.top = '42px';
+    load.style.left = '18px';
+
+
+    var loadText = document.createElement('div');
+    loadText.innerHTML = 'Підтвердіть, що ви людина';
+    loadText.style.color = 'white';
+    loadText.style.fontSize = '22px';
+    loadText.style.position = 'absolute';
+    loadText.style.top = '45px';
+    loadText.style.left = '70px';
+
+    var loadAnimation = document.createElement('style');
+    loadAnimation.innerHTML = `@keyframes IconCaptchaLoading {
+        0% {
+            transform: scale(1);
+        }
+
+        50% {
+            transform:  scale(1.3);
+        }
+
+        100% {
+            transform: scale(1);
+        }
+    }`;
+
+    document.head.appendChild(loadAnimation);
+    load.style.animation = '2s linear infinite IconCaptchaLoading';
+
+    var coverDiv = document.createElement('div');
+    coverDiv.style.display = 'flex';
+    coverDiv.style.width = '66%';
+    
+    coverDiv.appendChild(copyright);
+    coverDiv.appendChild(close);
+    dialog.appendChild(coverDiv);
+    dialog.appendChild(load);
+    dialog.appendChild(loadText)
+
+    document.body.appendChild(dialog);
+
+    dialog.showModal();
+
+};
+
+function CaptchaProccess(onfail, onsuccess, onclose) {
     function success() {
         while(captcha.firstChild) captcha.removeChild(captcha.lastChild);
 
@@ -228,34 +312,7 @@ function Captcha(onfail, onsuccess, onclose) {
         }, RandInt(2000, 3000));
     };
 
-    var dialog = document.createElement('dialog');
-    dialog.id = 'IconCaptchaDialog';
-    dialog.style.backgroundColor = '#4c4c4c';
-    dialog.style.border = '3px solid rgba(0, 0, 0, 0.2)';
-    dialog.style.borderRadius = '15px';
-    dialog.style.padding = '10px';
-    dialog.style.paddingLeft = '25px';
-    dialog.style.paddingRight = '25px';
-    dialog.style.zIndex = '2';
-
-    var close = document.createElement('img');
-    close.src = Icon('close');
-    close.style.width = '16px';
-    close.style.height = '16px';
-    close.style.position = 'relative';
-    close.style.top = '0.1%';
-    close.style.left = '107%';
-    close.addEventListener('click', function() {
-        dialog.close();
-        onclose();
-    });
-
-    var copyright = document.createElement('div');
-    copyright.innerHTML = '©Icon Captcha';
-    copyright.style.position = 'relative';
-    copyright.style.top = '0.1%';
-    copyright.style.left = '-7%';
-    copyright.style.color = 'rgba(255, 255, 255, 0.25)';
+    var dialog = document.getElementById('IconCaptchaDialog');
 
     var captcha = document.createElement('div');
     captcha.style.backgroundColor = '#ffffff';
@@ -278,14 +335,6 @@ function Captcha(onfail, onsuccess, onclose) {
     var a4 = document.createElement('img');
     var a5 = document.createElement('img');
 
-    var coverDiv = document.createElement('div');
-    coverDiv.style.display = 'flex';
-    coverDiv.style.width = '66%';
-    
-
-    coverDiv.appendChild(copyright);
-    coverDiv.appendChild(close);
-    dialog.appendChild(coverDiv);
     dialog.appendChild(text);
 
     captcha.appendChild(a1);
@@ -295,8 +344,6 @@ function Captcha(onfail, onsuccess, onclose) {
     captcha.appendChild(a5);
 
     dialog.appendChild(captcha);
-
-    document.body.appendChild(dialog);
 
     var icons = [getIcon(), getIcon(), getIcon()];
     var images = [a1, a2, a3, a4, a5];
@@ -331,11 +378,13 @@ function Captcha(onfail, onsuccess, onclose) {
         } else {
             image.addEventListener('click', fail);
         };
-    };
-
-    dialog.showModal();
+    };    
 };
 
 var CaptchaAttempts = 3;
 
 Captcha(function(){console.log("fail")}, function(){console.log("success")});
+
+var dialog = document.getElementById('IconCaptchaDialog')
+console.log(dialog.offsetWidth)
+console.log(dialog.offsetHeight)
